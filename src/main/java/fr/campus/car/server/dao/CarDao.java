6d65909aae05263ea436d96deb814c72b960fc18
Sandra <sandra.grillon@gmail.com>
2019-11-27 14:40:13 +0100

    Add Repository to CarDao

--- src/main/java/fr/campus/car/server/dao/CarDao.java
+++ src/main/java/fr/campus/car/server/dao/CarDao.java
@@ -1,16 +1,17 @@
 package fr.campus.car.server.dao;
 
 import fr.campus.car.server.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface CarDao {
-
+@Repository
+public interface CarDao extends JpaRepository<Car, Integer> {
     public List<Car> findAll();
     public Car findById(int id);
     public Car save(Car car);
     public Car update(int id, Car car);
     public Car delete(int id);
-
-
 }
